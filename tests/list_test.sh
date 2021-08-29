fd03b3b387a5cb1aee4c27ea5eeae33d8f73c36d
hopeman1102 <hopeman1102@gmail.com>
2021-08-29 14:21:44 +1200

    list and cdir tested

--- tests/list_test.sh
+++ tests/list_test.sh
@@ -3,7 +3,7 @@ NC='\033[0m' # No Color
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo "${GREEN}${bold}LISTING all files with F mode${NC}${normal}"
+echo "${GREEN}${bold}LISTING all files using LIST with F mode${NC}${normal}"
 echo "${GREEN}Commands from client:${NC}"
 cat << EOF
 USER user3
@@ -17,3 +17,35 @@ LIST F
 DONE
 EOF
 echo "${GREEN}${bold}✔ Files in current directory listed with F mode${NC}${normal}\n"
+
+echo "${GREEN}${bold}LISTING all files using LIST with V mode${NC}${normal}"
+echo "${GREEN}Commands from client:${NC}"
+cat << EOF
+USER user3
+LIST V
+DONE
+EOF
+echo "${GREEN}Message from Server:${NC}"
+../client/client << EOF
+USER user3
+LIST V
+DONE
+EOF
+echo "${GREEN}${bold}✔ Files in current directory listed with V mode${NC}${normal}\n"
+
+echo "${GREEN}${bold}Changing directory using CDIR and listing${NC}${normal}"
+echo "${GREEN}Commands from client:${NC}"
+cat << EOF
+USER user3
+CDIR dir1
+LIST V
+DONE
+EOF
+echo "${GREEN}Message from Server:${NC}"
+../client/client << EOF
+USER user3
+CDIR dir1
+LIST V
+DONE
+EOF
+echo "${GREEN}${bold}✔ Files in changed directory listed${NC}${normal}\n"
